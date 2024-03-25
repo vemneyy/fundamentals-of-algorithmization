541d0e2b7bee768533c4747f910e04b2ec1979d1
Semyon Saprykin <saprykinsemen@gmail.com>
2024-03-25 14:08:42 +0300

    first ex of eleventh lab

---
+++ lab-11-1/lab-11-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Person {
+    string surname;
+    int yearOfBirth;
+    int height;
+    int weight;
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // Создаем динамический экземпляр структуры Person
+    Person* personPtr = new Person{ "Иванов", 1939, 174, 72 };
+
+    // Получаем ссылку на созданный экземпляр, чтобы использовать синтаксис с точкой
+    Person& person = *personPtr;
+
+    // Выводим информацию о человеке, используя синтаксис с точкой
+    cout << "Фамилия: " << person.surname << endl;
+    cout << "Год рождения: " << person.yearOfBirth << endl;
+    cout << "Рост: " << person.height << " см" << endl;
+    cout << "Вес: " << person.weight << " кг" << endl;
+
+    // Освобождаем выделенную под структуру память
+    delete personPtr;
+
+    return 0;
+}
