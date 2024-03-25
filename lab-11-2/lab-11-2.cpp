d4f36bc9ba0a75b9053f93882e29fa70e63f758b
Semyon Saprykin <saprykinsemen@gmail.com>
2024-03-25 14:14:24 +0300

    second ex of eleventh lab

---
+++ lab-11-2/lab-11-2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int rows = 100;
+    const int cols = 200;
+    vector<vector<float>> matrix(rows, vector<float>(cols));
+    vector<float> flatMatrix(rows * cols);
+
+    srand(static_cast<unsigned>(time(0)));
+
+    // Заполнение матрицы и одномерного массива
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            float value = static_cast<float>(rand()) / RAND_MAX * 100; // Примерный диапазон [0, 100)
+            matrix[i][j] = value;
+            flatMatrix[i * cols + j] = value;
+        }
+    }
+
+    // Сортировка
+    for (size_t i = 0; i < flatMatrix.size(); i++) {
+        for (size_t j = 0; j < flatMatrix.size() - i - 1; j++) {
+            if (flatMatrix[j] > flatMatrix[j + 1]) {
+                swap(flatMatrix[j], flatMatrix[j + 1]);
+            }
+        }
+    }
+
+    cout << "Первая подматрица 10x20:" << endl; 
+
+    // Вывод первой подматрицы 10x20
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 20; ++j) {
+            cout << fixed << setprecision(4) << flatMatrix[i * 20 + j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
